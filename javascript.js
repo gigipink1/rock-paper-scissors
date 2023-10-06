eb5edc0bd18005838b3c462ef15afec2d1e8fc5f
Sascha Holmes <146263983+gigipink1@users.noreply.github.com>
2023-10-06 23:10:02 +0100

    Add playRound() to javascript.js
    adds a conditional function to return the winner for a
    single round which will be used to create a game of 5 rounds.

--- javascript.js
+++ javascript.js
@@ -7,4 +7,20 @@ function getUserChoice(){
     return prompt("Rock, Paper or Scissors?" ).toLowerCase();
     
 }
-           
+
+
+const computerSelection = getComputerChoice();
+const userSelection = getUserChoice()
+
+
+function playRound(comChoice , useChoice){
+if ((comChoice === "rock" && useChoice === "paper")
+|| (comChoice === "paper" && useChoice === "scissors")
+|| (comChoice === "scissors" && useChoice === "rock")) {
+return "You Win!";
+} else if (comChoice === useChoice) {
+return "It's a tie!";
+} else {
+ return "You Lose!";
+}
+}
