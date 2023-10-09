608bdf46329ff3d2929837ba029da674e463dc86
Sascha Holmes <146263983+gigipink1@users.noreply.github.com>
2023-10-09 20:10:35 +0100

    Refactor playRound(), improve format of javascript - - Changed playRound from results being returned to caller and replaced with DOM methods allowing the game to be played without alerts or console log.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
+const buttons = document.querySelectorAll('button'); 
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function () {PlayRound(button);} );
+
+
+});
+
+let playerCounter = 0;
+let comCounter = 0;
+
 function getComputerChoice() {
     const rpsChoices = ["rock", "paper", "scissors"];
     return rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
@@ -9,22 +20,31 @@ function getUserChoice(button){
 }
 
 
-function playRound(comChoice , useChoice){
-if ((comChoice === "rock" && useChoice === "paper")
-   || (comChoice === "paper" && useChoice === "scissors")
-   || (comChoice === "scissors" && useChoice === "rock")) {
-  return "You Win!";
-} else if (comChoice === useChoice) {
-  return "It's a tie!";
-} else {
-   return "You Lose!";
+function PlayRound (button) {
+    const playerChoice = button.id;
+    const comChoice = getComputerChoice();
+    const comStats = document.querySelector('.comStats');
+    const userStats = document.querySelector('.userStats');
+    const userPick = document.querySelector('.userPicked');
+    const comPick = document.querySelector('.comPicked')
+
+
+    if ((comChoice === "rock" && playerChoice === "paper")
+    || (comChoice === "paper" && playerChoice === "scissors")
+    || (comChoice === "scissors" && playerChoice === "rock")) {
+        userPick.textContent = `Your Choice was: ${playerChoice}`;
+        comPick.textContent =  `Computer Choice was:  ${comChoice}`;
+        userStats.textContent =  `Your Wins: ${++playerCounter}`;
+    } else if (comChoice === playerChoice) {
+        userPick.textContent = `Your Choice was: ${playerChoice}`; 
+        comPick.textContent =  `Computer Choice was:  ${comChoice}`;
+    } else {
+        userPick.textContent = `Your Choice was: ${playerChoice}`;
+        comPick.textContent =  `Computer Choice was:  ${comChoice}`;
+        comStats.textContent = `Computer Wins: ${++comCounter}`;
+    }
+
+
+
+
 }
-}
-
-const buttons = document.querySelectorAll('button'); 
-
-buttons.forEach((button) => {
-    button.addEventListener('click', function () {getUserChoice(button);} );
-
-
-});
